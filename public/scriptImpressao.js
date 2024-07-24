6b6959a84c3cac0e6e7465ac4b88c37ba2bb0df8
Glitch (glitch-hello-node) <none>
2024-07-24 02:11:30 +0000

    🚛🍍 Checkpoint ./server.js:38290350/4247 ./public/scriptImpressao.js:38290350/1496 ./public/index.html:38290350/1268 ./package.json:38290350/1201 ./public/style.css:38290350/4102

---
+++ public/scriptImpressao.js
@@ -0,0 +1,37 @@
+document.getElementById('botaoImpressaoCnpj').addEventListener('click', async function() {
+  const cepDigitado = document.getElementById('cep').value;
+  const cnpjDigitado = document.getElementById('cnpj').value;
+
+  try {
+    // Buscar dados do CEP na API ViaCEP
+    let resCep = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`);
+    let dataCep = await resCep.json();
+
+    if (dataCep.erro) {
+      throw new Error("CEP não encontrado");
+    }
+
+    // Buscar dados do CNPJ na API local
+    let resCnpj = await fetch(`/cnpj/${cnpjDigitado}`);
+    let dataCnpj = await resCnpj.json();
+
+    const endereco = `${dataCep.logradouro}, ${dataCep.bairro}, ${dataCep.localidade} - ${dataCep.uf}`;
+
+    // Preenchendo as células da tabela com os valores
+    document.getElementById('cnpj-td').textContent = cnpjDigitado;
+    document.getElementById('razao-social-td').textContent = dataCnpj.nome;
+    document.getElementById('endereco-td').textContent = endereco;
+    document.getElementById('telefone-td').textContent = dataCnpj.telefone;
+    document.getElementById('cep-td').textContent = cepDigitado;
+
+    // Exibindo a tabela
+    document.getElementById('data-table').style.display = 'block';
+  } catch (error) {
+    console.error(error);
+    alert("Erro ao buscar os dados. Por favor, verifique as informações digitadas e tente novamente.");
+  }
+});
+
+document.getElementById("botaoVoltarIndexImpressao").addEventListener("click", function() {
+  window.location.href = "/index.html";
+});
