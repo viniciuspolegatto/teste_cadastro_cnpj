69b1fe028171bc3274ab6916d41bd69e5a472222
Glitch (wooded-skinny-heat) <none>
2024-07-25 17:49:03 +0000

    😘🦄 Checkpoint ./public/index.html:38290350/3998 ./public/scriptImpressao.js:38290350/5697 ./server.js:38290350/2247 ./package.json:38290350/391 ./vercel.json:38290350/75 ./public/sebraetecSENAI.html:38290350/2984 ./public/scriptSebraetecSENAI.js:38290350/1277

--- public/scriptImpressao.js
+++ public/scriptImpressao.js
@@ -6,7 +6,6 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
     // Buscar dados do CEP na API ViaCEP
     let resCep = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`);
     let dataCep = await resCep.json();
-    console.log('Dados do CEP:', dataCep);
 
     if (dataCep.erro) {
       throw new Error("CEP não encontrado");
@@ -15,52 +14,6 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
     // Buscar dados do CNPJ na API local
     let resCnpj = await fetch(`/cnpj/${cnpjDigitado}`);
     let dataCnpj = await resCnpj.json();
-    console.log('Dados do CNPJ:', dataCnpj);
-
-    // Preencher a tabela com os dados obtidos
-    document.getElementById('cnpj-td').textContent = cnpjDigitado;
-    document.getElementById('razao-social-td').textContent = dataCnpj.nome;
-    document.getElementById('empresa-atividade-principal').textContent = dataCnpj.atividade_principal[0].text;
-    // Preencher mais campos conforme necessário...
-
-    // Armazenar os dados no localStorage
-    localStorage.setItem('dadosCnpj', JSON.stringify(dataCnpj));
-    localStorage.setItem('cepDigitado', cepDigitado);
-
-    // Exibir a tabela
-    document.getElementById('data-table').style.display = 'block';
-  } catch (error) {
-    console.error(error);
-    alert("Erro ao buscar os dados. Por favor, verifique as informações digitadas e tente novamente.");
-  }
-});
-
-
-
-/*
-document.getElementById('botaoImpressaoCnpj').addEventListener('click', async function() {
-  const cepDigitado = document.getElementById('cep').value;
-  const cnpjDigitado = document.getElementById('cnpj').value;
-
-  console.log('CEP Digitado:', cepDigitado);
-  console.log('CNPJ Digitado:', cnpjDigitado);
-
-  try {
-    // Buscar dados do CEP na API ViaCEP
-    let resCep = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json/`);
-    let dataCep = await resCep.json();
-    console.log('Dados do CEP:', dataCep);
-
-    if (dataCep.erro) {
-      throw new Error("CEP não encontrado");
-    }
-
-    // Buscar dados do CNPJ na API local
-    let resCnpj = await fetch(`/cnpj/${cnpjDigitado}`);
-    let dataCnpj = await resCnpj.json();
-    console.log('Dados do CNPJ:', dataCnpj);
-
-    const endereco = `${dataCep.logradouro}, ${dataCep.bairro}, ${dataCep.localidade} - ${dataCep.uf}`;
 
     // Preenchendo as células da tabela com os valores
     document.getElementById('cnpj-td').textContent = cnpjDigitado;
@@ -71,23 +24,18 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
     document.getElementById('empresa-municipio').textContent = dataCnpj.municipio;
     document.getElementById('empresa-situacao').textContent = dataCnpj.situacao;
     document.getElementById('telefone-td').textContent = dataCnpj.telefone;
-    document.getElementById('endereco-td').textContent = endereco;
+    document.getElementById('endereco-td').textContent = `${dataCep.logradouro}, ${dataCep.bairro}, ${dataCep.localidade} - ${dataCep.uf}`;
     document.getElementById('cep-td').textContent = cepDigitado;
 
-    // Armazenar os dados na sessionStorage
-    sessionStorage.setItem('dadosCnpj', JSON.stringify(dataCnpj));
-    sessionStorage.setItem('cepDigitado', cepDigitado);
-        
-    
     // Exibindo a tabela
     document.getElementById('data-table').style.display = 'block';
+
+    // Armazenar os dados no localStorage
+    localStorage.setItem('dadosCnpj', JSON.stringify(dataCnpj));
+    localStorage.setItem('cepDigitado', cepDigitado);
+
   } catch (error) {
     console.error(error);
     alert("Erro ao buscar os dados. Por favor, verifique as informações digitadas e tente novamente.");
   }
 });
-
-document.getElementById("botaoVoltarIndexImpressao").addEventListener("click", function() {
-  window.location.href = "/index.html";
-});
-*/
