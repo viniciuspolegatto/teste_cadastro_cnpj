71d7cb6ce0fa096ac2939fa45f46d3c24bfa1747
Glitch (wooded-skinny-heat) <none>
2024-07-26 12:14:52 +0000

    ⛴🚁 Checkpoint

--- public/scriptImpressao.js
+++ public/scriptImpressao.js
@@ -13,17 +13,17 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
 
     // Buscar dados do CNPJ na API local
     let resCnpj = await fetch(`/cnpj/${cnpjDigitado}`);
-    let dadosCnpj = await resCnpj.json();
+    let dataCnpj = await resCnpj.json();
 
     // Preenchendo as células da tabela com os valores
     document.getElementById('cnpj-td').textContent = cnpjDigitado;
-    document.getElementById('razao-social-td').textContent = dadosCnpj.nome;
-    document.getElementById('empresa-atividade-principal').textContent = dadosCnpj.atividade_principal[0].text;
-    document.getElementById('empresa-nome-fantasia').textContent = dadosCnpj.fantasia;
-    document.getElementById('empresa-logradouro').textContent = dadosCnpj.logradouro;
-    document.getElementById('empresa-municipio').textContent = dadosCnpj.municipio;
-    document.getElementById('empresa-situacao').textContent = dadosCnpj.situacao;
-    document.getElementById('telefone-td').textContent = dadosCnpj.telefone;
+    document.getElementById('razao-social-td').textContent = dataCnpj.nome;
+    document.getElementById('empresa-atividade-principal').textContent = dataCnpj.atividade_principal[0].text;
+    document.getElementById('empresa-nome-fantasia').textContent = dataCnpj.fantasia;
+    document.getElementById('empresa-logradouro').textContent = dataCnpj.logradouro;
+    document.getElementById('empresa-municipio').textContent = dataCnpj.municipio;
+    document.getElementById('empresa-situacao').textContent = dataCnpj.situacao;
+    document.getElementById('telefone-td').textContent = dataCnpj.telefone;
     document.getElementById('endereco-td').textContent = `${dataCep.logradouro}, ${dataCep.bairro}, ${dataCep.localidade} - ${dataCep.uf}`;
     document.getElementById('cep-td').textContent = cepDigitado;
 
@@ -31,22 +31,11 @@ document.getElementById('botaoImpressaoCnpj').addEventListener('click', async fu
     document.getElementById('data-table').style.display = 'block';
 
     // Armazenar os dados no localStorage
-    localStorage.setItem('dadosCnpj', JSON.stringify(dadosCnpj));
+    localStorage.setItem('dadosCnpj', JSON.stringify(dataCnpj));
     localStorage.setItem('cepDigitado', cepDigitado);
 
   } catch (error) {
     console.error(error);
     alert("Erro ao buscar os dados. Por favor, verifique as informações digitadas e tente novamente.");
   }
-  
-    const reportDiv = document.getElementById('report');
-  reportDiv.innerHTML =`
-  <p style="text-align: justify;">${dadosCnpj.nome}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero}, bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone}, e-mail ${dadosCnpj.email}, empresa neste ato representada por .... Cep Pessoal digitado: ${cepDigitado}, logradouro ${cepDigitado.logradouro} </P>
-  `;
-  
-
-  document.getElementById('voltar').addEventListener('click', function() {
-    window.location.href = '/index.html';
-  });
-  
 });
