879b50ab288d3e8d8d3b1b7d956d67d45b5b17e8
Glitch (wooded-skinny-heat) <none>
2024-07-26 13:13:47 +0000

    📖⛪️ Checkpoint ./public/scriptSebraetecImpressao.js:38290350/2164 ./public/index.html:38290350/6556 ./public/sebraetecImpressao.html:38290350/9

--- public/scriptSebraetecImpressao.js
+++ public/scriptSebraetecImpressao.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
+(function() {
+  // Obtendo os dados armazenados no localStorage
   const dadosCnpj = JSON.parse(localStorage.getItem('dadosCnpj'));
-  const cepDigitado = localStorage.getItem('cepDigitado');
+  const cepDigitado = JSON.parse(localStorage.getItem('cepDigitado'));
 
   if (!dadosCnpj || !cepDigitado) {
     alert("Dados não encontrados. Por favor, volte e preencha os dados novamente.");
@@ -8,14 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
     return;
   }
 
+  // Atualizando o conteúdo da div 'report' com as informações
   const reportDiv = document.getElementById('report');
-  reportDiv.innerHTML = 
-  `
-  <p style="text-align: justify;">${dadosCnpj.nome}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero}, bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone}, e-mail ${dadosCnpj.email}, empresa neste ato representada por .... Cep Pessoal digitado: ${cepDigitado}, logradouro ${cepDigitado.logradouro} </P>
+  reportDiv.innerHTML = `
+    <p style="text-align: justify;">
+      ${dadosCnpj.nome}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero}, 
+      bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone}, 
+      e-mail ${dadosCnpj.email}, empresa neste ato representada por .... Cep Pessoal digitado: ${cepDigitado.logradouro} 
+    </p>
   `;
-  
 
+  // Adicionando um evento de clique ao botão de voltar
   document.getElementById('voltar').addEventListener('click', function() {
     window.location.href = '/index.html';
   });
-});
+})();
