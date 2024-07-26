91fef8508cce5302af9fbdf9a27fe1506063434a
Glitch (wooded-skinny-heat) <none>
2024-07-26 13:02:00 +0000

    🦀🎾 Checkpoint ./public/index.html:38290350/12 ./public/scriptSebraetecImpressao.js:38290350/1023

---
+++ public/scriptSebraetecImpressao.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const dadosCnpj = JSON.parse(localStorage.getItem('dadosCnpj'));
+  const cepDigitado = localStorage.getItem('cepDigitado');
+
+  if (!dadosCnpj || !cepDigitado) {
+    alert("Dados não encontrados. Por favor, volte e preencha os dados novamente.");
+    window.location.href = '/index.html';
+    return;
+  }
+
+  const reportDiv = document.getElementById('report');
+  reportDiv.innerHTML = 
+  `
+  <p style="text-align: justify;">${dadosCnpj.nome}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero}, bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone}, e-mail ${dadosCnpj.email}, empresa neste ato representada por .... Cep Pessoal digitado: ${cepDigitado}, logradouro ${cepDigitado.logradouro} </P>
+  `;
+  
+
+  document.getElementById('voltar').addEventListener('click', function() {
+    window.location.href = '/index.html';
+  });
+});
