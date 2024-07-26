b43c6ba12487131fef8f86f8e358127e2aad417f
Glitch (wooded-skinny-heat) <none>
2024-07-26 15:00:56 +0000

    🐪🌸 Checkpoint ./public/scriptSebraetec.js:38290350/4471

--- public/scriptSebraetec.js
+++ public/scriptSebraetec.js
@@ -1,29 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const dadosCnpj = JSON.parse(localStorage.getItem('dadosCnpj'));
-  const cepDigitadoString = localStorage.getItem('cepDigitado');
+document.addEventListener("DOMContentLoaded", function () {
+  const dadosCnpj = JSON.parse(localStorage.getItem("dadosCnpj"));
+  const cepDigitadoString = localStorage.getItem("cepDigitado");
   const cepDigitado = cepDigitadoString ? JSON.parse(cepDigitadoString) : null;
+  const nomeCliente = localStorage.getItem("nomeCliente");
+  const numeroResidencia = localStorage.getItem("numeroResidencia");
+  const telefone = localStorage.getItem("telefone");
+  const emailpessoal = localStorage.getItem("email");
+  const cpf = localStorage.getItem("cpf");
 
   if (!dadosCnpj || !cepDigitado) {
-    alert("Dados não encontrados. Por favor, volte e preencha os dados novamente.");
-    window.location.href = '/index.html';
+    alert(
+      "Dados não encontrados. Por favor, volte e preencha os dados novamente."
+    );
+    window.location.href = "/index.html";
     return;
   }
 
-  const reportDiv = document.getElementById('report');
+  const reportDiv = document.getElementById("report");
   reportDiv.innerHTML = `
     <p style="text-align: justify;">
-      ${dadosCnpj.nome}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero}, bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone}, e-mail ${dadosCnpj.email}, empresa neste ato representada por .... Cep Pessoal digitado: ${cepDigitado.cep}, logradouro ${cepDigitado.logradouro}, ${cepDigitado.bairro}, ${cepDigitado.localidade} - ${cepDigitado.uf}
+      ${dadosCnpj.nome}, inscrita no CNPJ nº ${dadosCnpj.cnpj}, localizada na ${dadosCnpj.logradouro}, ${dadosCnpj.numero},
+      bairro ${dadosCnpj.bairro}, no município de ${dadosCnpj.municipio} - SP, CEP: ${dadosCnpj.cep}, telefone(s) ${dadosCnpj.telefone},
+      e-mail ${dadosCnpj.email}, empresa neste ato representada por ${nomeCliente}, brasileiro(a), com inscrição no CPF nº ${cpf},
+      residente .... Cep Pessoal digitado: ${cepDigitado.cep}, logradouro ${cepDigitado.logradouro}, ${cepDigitado.bairro}, ${cepDigitado.localidade} - ${cepDigitado.uf}
     </p>
   `;
-  
-  const reportDivCep = document.getElementById('reportCep');
+
+  const reportDivCep = document.getElementById("reportCep");
   reportDivCep.innerHTML = `
     <p style="text-align: justify;">
       ${cepDigitado.logradouro}, ${cepDigitado.bairro}, ${cepDigitado.localidade} - ${cepDigitado.uf}
     </p>
   `;
 
-  document.getElementById('voltar').addEventListener('click', function() {
-    window.location.href = '/index.html';
+  document.getElementById("voltar").addEventListener("click", function () {
+    window.location.href = "/index.html";
   });
 });
